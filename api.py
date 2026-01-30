b0b272e9e434aa581d53f302effde5d3101884c0
DabDragon21 <DabDragon21@gmail.com>
2026-01-30 00:43:33 -0500

    Initial Commit 9

--- api.py
+++ api.py
@@ -25,7 +25,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_openai import ChatOpenAI
-
 # -----------------------
 # App start
 # -----------------------
